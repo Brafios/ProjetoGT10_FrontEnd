63aa5cfbbc07d77022433a2e3f98c0a121a10917
unknown <clivekifumbi0161@digitalcollege.internal>
2025-05-12 12:00:59 -0300

    update footer

--- frontEnd_GT/src/App.jsx
+++ frontEnd_GT/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import './components/Footer/Footer.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/Register";
+import { Footer } from './components/Footer';
+// import Meunome from  "./pages/Register";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,14 +22,15 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/recuperar" element={<Recuperar />} />
+            {/* <Route path="/recuperar" element={<Recuperar />} />
             <Route path="/home" element={<Home />} />
             <Route path="/contato" element={<Contato />} />
-            <Route path="/associacao" element={<Associaco />} />
+            <Route path="/associacao" element={<Associaco />} /> */}
           </Routes>
         </main>
+        <Footer/>
 
-        <footer className="w-screen h-40 bg-gray-300 text-white">
+        {/* <footer className="w-screen h-40 bg-gray-300 text-white">
           <div className='logoCompleta'>
             <img src="./src/assets/img/LogoCompleta(1).png" alt="" />
             <p className='text-sm'>Copyright: @ 2020 Landify Ui kit <br></br> All rights reserved</p>
@@ -34,7 +38,7 @@ function App() {
 
             </div>
           </div>
-        </footer>
+        </footer> */}
       </>
     </BrowserRouter>
   )
