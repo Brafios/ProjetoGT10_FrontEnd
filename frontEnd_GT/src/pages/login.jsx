42a7f7fdff062e747e9df84b2d210e7a5024ee34
brafios8@gmail.com <brafios8@gmail.com>
2025-05-11 23:43:01 -0300

    login

--- frontEnd_GT/src/pages/login.jsx
+++ frontEnd_GT/src/pages/login.jsx
@@ -27,20 +27,20 @@ export default function Login() {
     
     <div className="w-screen h-150 bg-white flex justify-center items-center">
         <div className="flex flex-col items-center w-100">
-        <h1 className="text-3xl text-center text-white font-bold font-serif mt-3 mb-3">Login</h1>
-        <div className="flex justify-center">
-            <div className="flex flex-col">
-                <label htmlFor="" className="font-serif  font-semibold">Usuário ou E-mail</label>
-                <input  value={email} className="bg-gray-300 text-center w-150 h-10 mp-1 mb-3 " onChange={(e) => setEmail(e.target.value)} />
-                <div className="flex justify-between items-center mb-1">
-                    <label htmlFor="password" className="font-serif  font-semibold ">Senha</label>
-                    <a href="/recuperar" className="text-sm text-blue-500 hover:underline">Esqueceu a senha?</a>
+            <div className="flex  flex-col justify-center -mt-30">
+                <h1 className="text-3xl text-center font-bold font-serif mb-3">Login</h1>
+                <div className="flex flex-col">
+                    <label htmlFor="" className="font-serif  font-semibold">Usuário ou E-mail</label>
+                    <input  value={email} className="bg-gray-300 text-center w-150 h-10 mp-1 mb-3 " onChange={(e) => setEmail(e.target.value)} />
+                    <div className="flex justify-between items-center mb-1">
+                        <label htmlFor="password" className="font-serif  font-semibold ">Senha</label>
+                        <a href="/recuperar" className="text-sm text-blue-500 hover:underline">Esqueceu a senha?</a>
+                    </div>
+                    <input  type="password" value={password} className="bg-gray-300 text-center w-150 h-10  mb-3" onChange={(e) => setPassword(e.target.value)} />
                 </div>
-                <input  type="password" value={password} className="bg-gray-300 text-center w-150 h-10  mb-3" onChange={(e) => setPassword(e.target.value)} />
             </div>
-        </div>
-        <button className="bg-blue-500 text-white font-bold rounded-md mb-2 w-50 h-8" onClick={handleLogin}>Entrar</button>
-        <p>Não tem conta? <a href="/register" className="font-bold">Cadastre-se</a></p>
+            <button className="bg-blue-500 text-white font-bold rounded-md mb-2 w-50 h-8" onClick={handleLogin}>Entrar</button>
+            <p>Não tem conta? <a href="/register" className="font-bold">Cadastre-se</a></p>
         </div>
     </div>
   );
