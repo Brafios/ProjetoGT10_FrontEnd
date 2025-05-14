3c62cc39879736d0868250a5a918d3ef185c5e2d
vivian-costa <vivianaliciasilvacosta@gmail.com>
2025-05-14 10:24:00 -0300

    atualizando cadastro

--- frontEnd_GT/src/pages/Register.jsx
+++ frontEnd_GT/src/pages/Register.jsx
@@ -5,58 +5,42 @@ export default function Register() {
   return (
     <>
       <main className="bg-white min-h-screen flex flex-col justify-between font-sans">
-    <div>
-      <div className="bg-gray-200 py-6">
-          <h1 className="text-3xl font-bold font-serif mb-3 text-center">Cadastro</h1>
-        </div>
+        
+        <div className="flex flex-col justify-center items-center flex-1 py-10 bg-white">
+          <div className="flex flex-col justify-center items-center w-[750px] bg-gray-300 p-8 rounded">
+            <h1 className="text-3xl text-center font-bold font-serif mb-6">Cadastro</h1>
 
-  
-        <div className="flex-1 px-4 py-10">
-          <div className="w-[750px] mx-auto">
-            <form className="flex flex-col gap-6">
+            <form className="flex flex-col w-[400px] gap-4">
               {[
-                
-                { label: "Nome Completo", type: "text"  },
+                { label: "Nome Completo", type: "text" },
                 { label: "Usuário", type: "text" },
                 { label: "E-mail", type: "email" },
                 { label: "Função", type: "text" },
                 { label: "Senha", type: "password" },
-            
               ].map((field, index) => (
-                
                 <div key={index}>
-                  
-                  <label className="text-font-serif  font-semibold  block mb-1">{field.label} * </label> 
+                  <label className="font-serif font-semibold mb-1 block">{field.label}</label>
                   <input
                     type={field.type}
                     required
-                    className="bg-gray-300 text-black placeholder:text-gray-600 w-[700px] h-[40px] block px-3"
+                    className="bg-white text-center h-10 mb-2 rounded w-full"
                   />
                 </div>
               ))}
-             
 
-        
               <button
                 type="submit"
-                className="bg-gray-500 text-white font-bold rounded-md mb-2 w-40 h-8 hover:bg-gray-800"
+                className="bg-gray-500 text-white font-bold rounded-md w-[150px] h-10 self-center"
               >
-                Enviar
+                Cadastrar
               </button>
 
-          
-              <p className=" text-center mt-2  ">
-                Já tem conta?{' '}
-                <Link to="/" className="text-blue-600 underline font-bold">
-                  Entrar
-                </Link>
+              <p className="mt-4 text-center">
+                Já tem conta? <Link to="/" className="font-bold text-blue-600">Entrar</Link>
               </p>
             </form>
           </div>
         </div>
-
-    </div>
-        
 
       </main>
     </>
