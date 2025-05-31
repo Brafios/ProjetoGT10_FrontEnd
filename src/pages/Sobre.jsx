504f348d0a0ec6ef0511389ead66a89aff26bd15
brafios8@gmail.com <brafios8@gmail.com>
2025-05-31 16:13:43 -0300

    alterações

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -6,10 +6,10 @@ export default function Sobre() {
         <div className="h-screen w-screen flex justify-center items-center mt-28">
             <div className="flex flex-col items-center w-full max-w-3xl bg-gray-300 p-8 rounded shadow-lg">
                 <h2 className="text-3xl text-center font-bold font-serif mb-6">
-                    Nossa História começou em 2025 procurando ajudar associacoes e empresários
+                    Nossa História começou em 2025 procurando ajudar associações e empresários
                 </h2>
                 <p className="text-lg text-justify font-serif mb-6 text-gray-700">
-                    Federação do Comércio foi criada para ajudar associações e empresários ganhar visibilidade e destaque até mesmo tratando sobre questões governamentais através de reuniões com propio governo.
+                    Federação do Comércio foi criada para ajudar associações e empresários ganhar visibilidade e destaque.
                 </p>
             </div>
         </div>
