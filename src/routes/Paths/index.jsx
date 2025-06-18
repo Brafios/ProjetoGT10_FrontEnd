e1c8b576f4e91510d10e9799ff1b3431056c684f
brafios8@gmail.com <brafios8@gmail.com>
2025-06-18 20:17:22 -0300

    rota que estava faltando

--- src/routes/Paths/index.jsx
+++ src/routes/Paths/index.jsx
@@ -10,6 +10,7 @@ import Registro from "../../pages/Registro";
 import Recuperar from "../../pages/Recuperar";
 import Associacoes from "../../pages/Associacoes"
 import Contato from "../../pages/Contato"
+import AtualizarSenha from "../../pages/AtualizarSenha"; 
 
 const Paths = () => {
     return ( 
@@ -23,6 +24,7 @@ const Paths = () => {
                     <Route path="/recuperar" element={<Recuperar/>}/>
                     <Route path="/associacoes" element={<Associacoes/>}/>
                     <Route path="/contato" element={<Contato/>}/>
+                    <Route path="/update-password" element={<AtualizarSenha />} />
                 </Route>
                 <Route path="/EmConstrucao" element={<EmConstrucao/>}/>
                 <Route path="/*" element={<NotFound />} />
